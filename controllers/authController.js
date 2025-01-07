7bf04071cb42c2aa873ac949053373e7c9ad8ad8
Dery <deryaprianto4@gmail.com>
2025-01-07 14:09:39 +0700

    testing serverless

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,46 +1,13 @@
-const jwt = require('jsonwebtoken');
-const {
-	signupSchema,
-	signinSchema,
-} = require('../middlewares/validator');
-const User = require('../models/usersModel');
-const { doHash, doHashValidation } = require('../utils/hashing');
+// const jwt = require('jsonwebtoken');
+// const {
+// 	signupSchema,
+// 	signinSchema,
+// } = require('../middlewares/validator');
+// const User = require('../models/usersModel');
+// const { doHash, doHashValidation } = require('../utils/hashing');
 
 async function signup(req, res) {
-	const { fullname, email, password } = req.body;
-	try {
-		const { error, value } = signupSchema.validate({ fullname, email, password });
-
-		if (error) {
-			return res
-				.status(401)
-				.json({ success: false, message: error.details[0].message });
-		}
-		const existingUser = await User.findOne({ email });
-
-		if (existingUser) {
-			return res
-				.status(401)
-				.json({ success: false, message: 'User already exists!' });
-		}
-
-		const hashedPassword = await doHash(password, 12);
-
-		const newUser = new User({
-			fullname,
-			email,
-			password: hashedPassword,
-		});
-		const result = await newUser.save();
-		result.password = undefined;
-		res.status(201).json({
-			success: true,
-			message: 'Your account has been created successfully',
-			result,
-		});
-	} catch (error) {
-		console.log(error);
-	}
+	res.json({ message: 'Hello from the server' });
 }
 
 // exports.signup = async (req, res) => {
