4d534f3f1779a91ec16bde48da873a5c0f6e691f
Dery <deryaprianto4@gmail.com>
2025-01-07 12:52:31 +0700

    test deploy

--- api/index.js
+++ api/index.js
@@ -1,44 +1,10 @@
 const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-
-const authRouter = require('./routers/authRouter');
-const userRouter = require('./routers/userRouter');
-
-// Konfigurasi CORS
-const corsOptions = {
-	origin: 'https://wedding-eight-pi.vercel.app', // Ganti dengan asal yang diperbolehkan
-	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Metode yang diizinkan
-	credentials: true, // Jika menggunakan cookie atau header otentikasi
-};
-
 const app = express();
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-app.use(helmet());
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-mongoose
-	.connect(process.env.MONGO_URI)
-	.then(() => {
-		console.log('Database connected');
-	})
-	.catch((err) => {
-		console.log(err);
-	});
-
-app.use('/api/auth', authRouter);
-app.use('/api', userRouter);
+// Middleware atau logika lainnya
 app.get('/', (req, res) => {
-	res.json({ message: 'Hello from the server' });
+  res.json({ message: 'Hello World from Express.js' });
 });
 
-app.listen(process.env.PORT, () => {
-	console.log('listening...');
-});
-
+// Ekspor handler
 module.exports = app;
