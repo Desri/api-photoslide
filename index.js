fdfe049877b13534d609a4865f20f1e820df08ad
Dery <deryaprianto4@gmail.com>
2025-01-07 09:37:32 +0700

    fix cors

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ const userRouter = require('./routers/userRouter');
 // const postsRouter = require('./routers/postsRouter');
 
 // Konfigurasi CORS
-const corsOptions = {
-	origin: 'https://wedding-eight-pi.vercel.app', // Ganti dengan asal yang diperbolehkan
-	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Metode yang diizinkan
-	credentials: true, // Jika menggunakan cookie atau header otentikasi
-};
+// const corsOptions = {
+// 	origin: 'https://wedding-eight-pi.vercel.app', // Ganti dengan asal yang diperbolehkan
+// 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Metode yang diizinkan
+// 	credentials: true, // Jika menggunakan cookie atau header otentikasi
+// };
 
 const app = express();
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.options('*', cors());
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
