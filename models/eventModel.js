2cf392062ee6d9c5e78902d97cd47195a446d800
Dery <deryaprianto4@gmail.com>
2025-01-11 13:54:49 +0700

    ss

--- models/eventModel.js
+++ models/eventModel.js
@@ -15,15 +15,63 @@ const eventSchema = mongoose.Schema(
 		eventType: {
 			type: String,
 		},
-		planssss: {
-			type: String,
-			default: "Dery h",
-		},
 		plan: {
 			type: mongoose.Types.ObjectId,
 			ref: 'User'
 		},
-		
+		appearance: {
+			language: {
+				type: String,
+				default: ""
+			},
+			caption: {
+				type: String,
+				default: ""
+			},
+			textPostBackground: {
+				type: Boolean,
+			},
+			eventLogo: {
+				type: Boolean,
+			}
+		},
+		slideShow: {
+			durationImage: {
+				type: String,
+				default: ""
+			},
+			durationVideo: {
+				type: String,
+				default: ""
+			},
+			hideSlideshowQR: {
+				type: Boolean,
+			},
+			hideVideoSound: {
+				type: Boolean,
+			}
+		},
+		moderation: {
+			permissions: {
+				type: String,
+				default: ""
+			},
+			disableGuestsDownload: {
+				type: Boolean,
+			}
+		},
+		collaborator: [{
+			email: {
+			  	type: String,
+			  	trim: true,
+				unique: true,
+				default: ""
+			},
+			role: {
+				type: String,
+				default: ""
+			}
+		}],
 	}
 );
 
