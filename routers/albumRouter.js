9ca471923d97211c0cab21de6222e6a7ec4c6751
Dery <deryaprianto4@gmail.com>
2025-01-22 15:10:53 +0700

    x

--- routers/albumRouter.js
+++ routers/albumRouter.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post('/create-album', identifier, albumController.createAlbums)
 router.get('/:slug', identifier, albumController.listAlbums)
-router.delete('/remove', identifier, albumController.removeAlbums)
+router.post('/remove', identifier, albumController.removeAlbums)
 
 module.exports = router;
