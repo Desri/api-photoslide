a07327e1807d4bb41577a66cb4e7eeb1176b4ef8
Dery <deryaprianto4@gmail.com>
2025-01-25 22:28:27 +0700

    clear code not use

--- api/index.js
+++ api/index.js
@@ -6,30 +6,12 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-// const corsOptions = {
-// 	origin: 'https://wedding-eight-pi.vercel.app',
-// 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-// 	credentials: true,
-// };
-
 const app = express();
 app.use(cors())
-// app.use(cors(corsOptions));
-// app.options('*', cors(corsOptions));
-
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Middleware untuk menambahkan header CORS secara global
-// app.use((req, res, next) => {
-// 	res.setHeader('Access-Control-Allow-Origin', 'https://wedding-eight-pi.vercel.app'); // Ganti dengan domain yang diperbolehkan
-// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-// 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-// 	res.setHeader('Access-Control-Allow-Credentials', 'true'); // Untuk mengizinkan cookie/credentials
-// 	next();
-// });
 
 const albumRouter = require('./../routers/albumRouter');
 const eventRouter = require('./../routers/eventRouter');
